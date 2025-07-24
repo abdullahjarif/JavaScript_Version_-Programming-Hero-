231672d87e819ecdef4b31694a037210b88ebf1c
Abdullah Jarif <155164388+abdullahjarif@users.noreply.github.com>
2025-07-25 00:20:55 +0600

    Create shortcut.js

---
+++ Module-18_fundamental concepts of conditionals/shortcut.js
@@ -0,0 +1,19 @@
+const isLeader = true;
+
+// if(isLeader){
+//     console.log("leader asche! chair charo");
+    
+// }else{
+//     console.log("vitore dukte dibi na.");
+    
+// }
+
+const isPassed = false;
+// if(isPassed === true)
+if(!!isPassed){
+    console.log("toke biye diya dimu");
+    
+}else{
+    console.log("r o porashuna kor");
+    
+}
