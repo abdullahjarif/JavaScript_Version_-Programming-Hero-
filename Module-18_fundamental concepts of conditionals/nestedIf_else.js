6b2029fa80f237a89f9a79b76f7c4ee93ca4e5a3
Abdullah Jarif <155164388+abdullahjarif@users.noreply.github.com>
2025-07-24 23:28:22 +0600

    Add files via upload

---
+++ Module-18_fundamental concepts of conditionals/nestedIf_else.js
@@ -0,0 +1,19 @@
+let haveMoney = 60;
+
+if(haveMoney>300){
+    console.log("Bro! u are rich");
+}else{
+    if(haveMoney>=150){
+        console.log("you are not enough rich");
+        
+    }else{
+        if(haveMoney>=50){
+            console.log("you are rich but very poor");
+            
+        }
+        else{
+            console.log("you are not enough rich and very poor");
+            
+        }
+    }
+}
