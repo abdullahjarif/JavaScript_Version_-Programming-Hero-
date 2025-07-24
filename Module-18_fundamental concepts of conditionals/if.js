3a1b1480948f25cf276f03911cb6762c0f6ee8d8
Abdullah Jarif <155164388+abdullahjarif@users.noreply.github.com>
2025-07-24 10:04:04 +0600

    Add files via upload

---
+++ Module-18_fundamental concepts of conditionals/if.js
@@ -0,0 +1,48 @@
+/**
+ * structure of if else
+ * 
+ * if(condition){
+ *  run on 
+ * }
+ */
+
+// if(3<10){
+if(13<10){
+    console.log("I have e smaller value"); 
+}
+
+var biriyaniPrice = 150;
+if(biriyaniPrice<300){
+    console.log("give me some biriyani");
+}
+
+var weight = 30;
+if(weight < 20){
+    console.log("I will carry it by myself");
+    
+}else{
+    console.log("I will rent a rickshaw");
+    
+}
+
+let money = 510;
+if(money > 300){
+    console.log("go to cineplex");
+    
+}else{
+    console.log("do nothing");
+    
+}
+
+let a=5;
+if(a<=5){
+    console.log("execution this code"); 
+}
+
+/**
+ * if(condition){
+ *  execute code if condition above is true
+ * } else{
+ *  execute code if condition is false
+ * }
+ */
