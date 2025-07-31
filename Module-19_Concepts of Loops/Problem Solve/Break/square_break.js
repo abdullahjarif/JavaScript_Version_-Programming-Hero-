fecbb663c1cab647961f94d2737d29c8d4659e2f
Abdullah Jarif <155164388+abdullahjarif@users.noreply.github.com>
2025-07-31 20:46:15 +0600

    Update square_break.js

--- Module-19_Concepts of Loops/Problem Solve/Break/square_break.js
+++ Module-19_Concepts of Loops/Problem Solve/Break/square_break.js
@@ -1,17 +1,8 @@
-for (let i = 2; i <= 100; i++) {
-    const sqrt = Math.sqrt(i);
-    if (sqrt === Math.floor(sqrt)) {
-        console.log(`Found square number: ${i}`);
-        break;
+for(let i=1; i<=100; i++){
+    for(let j=1; j*j<=i; j++){
+        if(j*j === i){
+            console.log("square root number found! ",i);
+            break;
+        }
     }
-    console.log(i);
-}
-
-for (let j = 2; j <= 100; j++) {
-    const sqrt = Math.sqrt(j);
-    if (sqrt === Math.floor(sqrt)) {
-        console.log(`Found square number: ${j}`);
-        continue;
-    }
-    console.log(j);
 }
